425cac49f5c3f9095db00b866bc72edb7dc64b02
xna <xna00@qq.com>
2021-02-22 11:17:18 +0800

    init

---
+++ precache-manifest.f2e98e9255c7996bc7e500b1cec9c177.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22745c44843254b20c8",
+    "url": "css/app.8a05e0fa.css"
+  },
+  {
+    "revision": "8b031ef47f7719caee8a4a083a90d57d",
+    "url": "favicon.svg"
+  },
+  {
+    "revision": "016b53053800279c09ee7a750112e5f3",
+    "url": "index.html"
+  },
+  {
+    "revision": "a22745c44843254b20c8",
+    "url": "js/app.88fbefcb.js"
+  },
+  {
+    "revision": "bcb69b2af8c9ae4ba26b",
+    "url": "js/chunk-vendors.db3a9bee.js"
+  },
+  {
+    "revision": "2389b9051ff3dfa899176100d61983f5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
